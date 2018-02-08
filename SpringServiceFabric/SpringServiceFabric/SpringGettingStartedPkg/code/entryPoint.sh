579e372b12122b3ab6b5893dc0fdc18acd06a1c2
Menghua Xiao <menxiao@microsoft.com>
2018-02-08 02:18:00 +0000

    Add service fabric configuration

---
+++ SpringServiceFabric/SpringServiceFabric/SpringGettingStartedPkg/code/entryPoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#===============================================================================
+#          FILE:  entryPoint.sh
+# 
+#   DESCRIPTION:  
+# 
+#        AUTHOR: YOUR NAME, 
+#       CREATED: 02/07/2018 08:48:46 AM UTC
+#===============================================================================
+
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+java -jar gs-spring-boot-0.1.0.jar
+
